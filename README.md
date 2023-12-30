1dd98a37bee6d05223957974b1d5d8c9c92ec04e
ThomasKnoops <56110999+ThomasKnoops@users.noreply.github.com>
2023-12-30 14:52:57 +0100

    update: readme and about

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-Edit profile is 'hidden' under your own name when logged in.
+# Project Name
 
-Sources:
-https://stackoverflow.com/questions/20633310/how-to-get-random-text-from-lorem-ipsum-in-php
+Description of your project.
+
+## Edit Profile
+
+The "Edit profile" option is hidden under your own name when logged in.
+
+## Sources
+
+- I used the Laravel documentation for most of the code.
+- I used the resources from the Canvas Crusus for most of the code.
+
+### Other Sources
+
+- [YouTube Tutorial](https://www.youtube.com/watch?v=utm2YVJEM9c)
+- [Stack Overflow: How to get random text from Lorem Ipsum in PHP](https://stackoverflow.com/questions/20633310/how-to-get-random-text-from-lorem-ipsum-in-php)
+- [Bootstrap Integration with Laravel](https://marketsplash.com/tutorials/laravel/how-to-integrate-bootstrap-with-laravel/)
+- [Laravel Many-to-Many Relationship](https://www.iankumu.com/blog/laravel-many-to-many-relationship/)
+- [Running Laravel on Windows with XAMPP](https://divpusher.com/blog/how-to-run-laravel-on-windows-with-xampp/)
+- [Laravel Database Seeding Tutorial](https://makitweb.com/laravel-database-seeding-tutorial/)
--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -12,10 +12,15 @@
                     <p>Edit profile is 'hidden' under your own name when logged in.</p>
                     <p>Sources:</p>
                     <ul>
-                        <li><a href="https://laravel.com/docs/8.x/eloquent">https://laravel.com/docs/8.x/eloquent</a></li>
+                        <li><p>I used the Laravel documentation for most of the code.</p></li>
+                        <li><p>I used the resources from the Canvas Crusus for most of the code.</p></li>
+                        <li><p>These are other sources than the 2 mentioned above:</p></li>
                         <li><a href="https://www.youtube.com/watch?v=utm2YVJEM9c">https://www.youtube.com/watch?v=utm2YVJEM9c</a></li>
                         <li><a href="https://stackoverflow.com/questions/20633310/how-to-get-random-text-from-lorem-ipsum-in-php">https://stackoverflow.com/questions/20633310/how-to-get-random-text-from-lorem-ipsum-in-php</a></li>
-                    
+                        <li><a href="https://marketsplash.com/tutorials/laravel/how-to-integrate-bootstrap-with-laravel/">https://marketsplash.com/tutorials/laravel/how-to-integrate-bootstrap-with-laravel/</a></li>
+                        <li><a href="https://www.iankumu.com/blog/laravel-many-to-many-relationship/">https://www.iankumu.com/blog/laravel-many-to-many-relationship/</a></li>
+                        <li><a href="https://divpusher.com/blog/how-to-run-laravel-on-windows-with-xampp/">https://divpusher.com/blog/how-to-run-laravel-on-windows-with-xampp/</a></li>
+                        <li><a href="https://makitweb.com/laravel-database-seeding-tutorial/">https://makitweb.com/laravel-database-seeding-tutorial/</a></li>                    
                 </div>
             </div>
         </div>
